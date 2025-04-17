ecc6a2cfb74e0ceaba313089dfb942e37930d31d
Abhigya Krishna <abhigyakrishna21@gmail.com>
2025-04-18 00:46:42 +0530

    feat: grid-engine impl

--- packages/nextjs/game/main.ts
+++ packages/nextjs/game/main.ts
@@ -3,21 +3,42 @@ import { Game as MainGame } from "./scenes/Game";
 import { GameOver } from "./scenes/GameOver";
 import { MainMenu } from "./scenes/MainMenu";
 import { Preloader } from "./scenes/Preloader";
-import { AUTO, Game } from "phaser";
+import GridEngine from "grid-engine";
+import Phaser from "phaser";
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Phaser.Types.Core.GameConfig = {
-  type: AUTO,
+  type: Phaser.AUTO,
   width: 1024,
   height: 768,
   parent: "game-container",
   backgroundColor: "#028af8",
+  scale: {
+    mode: Phaser.Scale.FIT,
+  },
+  plugins: {
+    scene: [
+      {
+        key: "gridEngine",
+        plugin: GridEngine,
+        mapping: "gridEngine",
+      },
+    ],
+  },
   scene: [Boot, Preloader, MainMenu, MainGame, GameOver],
+  input: {
+    mouse: {
+      preventDefaultWheel: false,
+    },
+  },
 };
 
-const StartGame = (parent: string) => {
-  return new Game({ ...config, parent });
+const StartGame = (parent: string | undefined) => {
+  return new Phaser.Game({
+    ...config,
+    parent: parent ? parent : config.parent,
+  });
 };
 
 export default StartGame;
