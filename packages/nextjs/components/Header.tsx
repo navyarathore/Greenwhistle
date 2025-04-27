9cdc9cccd6b4523e7a88ae21397c64a59a98236d
vaibhavkothari33 <vaibhavkothari50@gmail.com>
2025-04-28 00:00:59 +0530

    feat: made the wishlist data coming from blockchain removed from navbar

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -52,27 +52,27 @@ export const menuLinks: HeaderMenuLink[] = [
     icon: <BookOpenIcon className="h-5 w-5" />,
     protected: true,
   },
-  {
-    label: "Wishlist",
-    href: "/wishlist",
-    icon: (
-      <svg
-        className="w-5 h-5 mr-1"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M19 21L12 17L5 21V5C5 4.46957 5.21071 3.96086 5.58579 3.58579C5.96086 3.21071 6.46957 3 7 3H17C17.5304 3 18.0391 3.21071 18.4142 3.58579C18.7893 3.96086 19 4.46957 19 5V21Z"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    ),
-    protected: true,
-  },
+  // {
+  //   label: "Wishlist",
+  //   href: "/wishlist",
+  //   icon: (
+  //     <svg
+  //       className="w-5 h-5 mr-1"
+  //       viewBox="0 0 24 24"
+  //       fill="none"
+  //       stroke="currentColor"
+  //       xmlns="http://www.w3.org/2000/svg"
+  //     >
+  //       <path
+  //         d="M19 21L12 17L5 21V5C5 4.46957 5.21071 3.96086 5.58579 3.58579C5.96086 3.21071 6.46957 3 7 3H17C17.5304 3 18.0391 3.21071 18.4142 3.58579C18.7893 3.96086 19 4.46957 19 5V21Z"
+  //         strokeWidth="2"
+  //         strokeLinecap="round"
+  //         strokeLinejoin="round"
+  //       />
+  //     </svg>
+  //   ),
+  //   protected: true,
+  // },
 ];
 
 export const Header = () => {
